0a11b8275a5a8a18441006f6fc3c5ed5fc61d6d4
Keith Butler <20089137@mail.wit.ie>
2022-09-01 15:31:49 +0100

    wrapped battery notifier in function

--- battery_notifier/app.py
+++ battery_notifier/app.py
@@ -5,20 +5,24 @@ import time
 
 app_name = title = 'Battery Warning'
 
-while True:
-    if battery.status['isCharging'] == False and battery.status['percentage'] <= 20:
-        notification.notify(
-            title = title,
-            message = 'Battery is low better get charging',
-            app_name = app_name,
-            toast = False,
-        )
-    elif battery.status['isCharging'] == True and battery.status['percentage'] >= 90:
-        notification.notify(
-            title = title,
-            message = 'Battery is full got to perserve battery health',
-            app_name = app_name,
-            toast = False,
-        )
+def main():
+    while True:
+        if battery.status['isCharging'] == False and battery.status['percentage'] <= 20:
+            notification.notify(
+                title = title,
+                message = 'Battery is low better get charging',
+                app_name = app_name,
+                toast = False,
+            )
+        elif battery.status['isCharging'] == True and battery.status['percentage'] >= 90:
+            notification.notify(
+                title = title,
+                message = 'Battery is full got to perserve battery health',
+                app_name = app_name,
+                toast = False,
+            )
 
-    time.sleep(60)
+        time.sleep(60)
+
+if __name__ == "__main__":
+    main()
