36b4d5af92e7749732ddc0332f166013fa7d1a9f
Keith Butler <20089137@mail.wit.ie>
2022-09-25 23:22:15 +0100

    added 404 template

--- pathfindingVisualiser/pathfindingVisualiser/settings.py
+++ pathfindingVisualiser/pathfindingVisualiser/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-tm^cn0-6s&7ahl+#()xp$b9z%m&uyvhe+y75wktoc379zhk*7*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
