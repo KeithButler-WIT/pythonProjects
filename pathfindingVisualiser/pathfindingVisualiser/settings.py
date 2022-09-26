e04f6b5fd5235c8f420301af87b2a1726848fb5a
Keith Butler <20089137@mail.wit.ie>
2022-09-26 22:59:13 +0100

    Update settings.py

--- pathfindingVisualiser/pathfindingVisualiser/settings.py
+++ pathfindingVisualiser/pathfindingVisualiser/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-tm^cn0-6s&7ahl+#()xp$b9z%m&uyvhe+y75wktoc379zhk*7*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
