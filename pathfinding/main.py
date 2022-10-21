6c401f9dc774b50b33bae353e0af19001d6c038f
Keith Butler <20089137@mail.wit.ie>
2022-10-21 17:24:16 +0100

    Updated README
    changed readme extention back to md and started using a template

--- pathfinding/main.py
+++ pathfinding/main.py
@@ -11,9 +11,13 @@ pygame.display.set_caption("Pathfinding Visualiser using pygame")
 # pygame.display.set_caption("A* Pathfinding")
 # pygame.display.set_caption("Dijkstra Pathfinding")
 
+
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 
+pygame.init()
 
 def get_clicked_pos(pos, rows, width):
     gap = width //rows
@@ -80,16 +84,41 @@ def start_game(win, width, algo):
     pygame.quit()
 
 
-def button(x,y,w,h,algo):
-    pos = pygame.mouse.get_pos()
+def text_objects(text, font):
+    TextSurface = font.render(text, True, BLACK)
+    return TextSurface, TextSurface.get_rect()
+
+
+def button(msg, x, y, w, h, ic, ac, algo):
+    mouse_pos = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
 
-    if pos[0] > x and pos[0] < x + w and pos[1] > y and pos[1] < y + h:
-       if click[0] == 1 and algo == "aStar":
-         start_game(WIN, WIDTH, "aStar")
-       if click[0] == 1 and algo == "dijkstra":
-         start_game(WIN, WIDTH, "dijkstra")
-    pygame.draw.rect(WIN, WHITE, (x,y,w,h))
+    if mouse_pos[0] > x and mouse_pos[0] < x + w and mouse_pos[1] > y and mouse_pos[1] < y + h:
+        pygame.draw.rect(WIN, ac,(x,y,w,h))
+        if click[0] == 1 and algo == "aStar":
+            start_game(WIN, WIDTH, "aStar")
+        if click[0] == 1 and algo == "dijkstra":
+            start_game(WIN, WIDTH, "dijkstra")
+    else:
+        pygame.draw.rect(WIN, ic,(x,y,w,h))
+
+    smallText = pygame.font.Font("freesansbold.ttf",20)
+    textSurf, textRect = text_objects(msg, smallText)
+    textRect.center = ( (x+(w/2)), (y+(h/2)) )
+    WIN.blit(textSurf, textRect)
+
+
+# def button_old(x,y,w,h,algo):
+#     pos = pygame.mouse.get_pos()
+#     click = pygame.mouse.get_pressed()
+
+#     smallText = pygame.font.Font("freesansbold.ttf",20)
+#     textSurf, textRect = text_objects("GO!", smallText)
+#     textRect.center = ( (w+(h/2)), (450+(50/2)) )
+#     WIN.blit(textSurf, textRect)
+
+#     if pos[0] > x and pos[0] < x + w and pos[1] > y and pos[1] < y + h:
+#         pygame.draw.rect(WIN, WHITE, (x,y,w,h))
 
 
 def main():
@@ -102,8 +131,9 @@ def menu():
     WIN.blit(WIN, (0, 0))
 
 
-    button(WIDTH/4,WIDTH/2 + WIDTH/4,80,40,"aStar")
-    button(WIDTH/4 + WIDTH/3,WIDTH/2 + WIDTH/4,80,40,"dijkstra")
+    button("aStar", WIDTH/4, WIDTH/2 + WIDTH/4, 80, 40, WHITE, GREEN,  "aStar")
+    button("Dijkstra", WIDTH/4 + WIDTH/3,WIDTH/2 + WIDTH/4, 80, 40, WHITE, GREEN,  "dijkstra")
+    # button(WIDTH/4 + WIDTH/3,WIDTH/2 + WIDTH/4,80,40,"dijkstra")
 
     for event in pygame.event.get():
        if event.type == pygame.QUIT:
